dc906e263d469760b213209dd5c3a8b0a5b7f747
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-10-31 23:27:51 -0300

    test: ensure RequireField returns MissingParamError if the field is not provided

---
+++ tests/presentation/helpers/validation/required-field-validation.spec.ts
@@ -0,0 +1,26 @@
+import { MissingParamError } from "../../../../src/presentation/errors/missing-param-error";
+import { RequiredFieldValidation } from "../../../../src/presentation/helpers/validators/required-field-validation/required-field-validation";
+
+const makeSut = () => {
+  const sut = new RequiredFieldValidation("email");
+
+  return {
+    sut,
+  };
+};
+
+describe("RequiredField validation", () => {
+  test("should return MissingParamError if the field is not provided", () => {
+    const { sut } = makeSut();
+    const res = sut.validate({});
+
+    expect(res).toStrictEqual(new MissingParamError("email"));
+  });
+
+  test("should return null if validation succeeds", () => {
+    const { sut } = makeSut();
+    const res = sut.validate({});
+
+    expect(res).toStrictEqual(new MissingParamError("email"));
+  });
+});
