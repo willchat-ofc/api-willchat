678232b81a20a1a5a152f936a0ee1b9f72234539
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-12-10 12:20:24 -0300

    feat: esure DeleteKeyController returns serverError if validator throws

--- src/presentation/controller/delete-key.ts
+++ src/presentation/controller/delete-key.ts
@@ -1,4 +1,4 @@
-import { badRequest } from "../helpers/http-helper";
+import { badRequest, serverError } from "../helpers/http-helper";
 import type { Controller } from "../protocols/controller";
 import type { HttpRequest, HttpResponse } from "../protocols/http";
 import type { Validation } from "../protocols/validation";
@@ -7,7 +7,11 @@ export class DeleteKeyController implements Controller {
   public constructor(private readonly validator: Validation) {}
 
   public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validator.validate(httpRequest.header);
-    if (error) return badRequest(error);
+    try {
+      const error = this.validator.validate(httpRequest.body);
+      if (error) return badRequest(error);
+    } catch (err) {
+      return serverError();
+    }
   }
 }
