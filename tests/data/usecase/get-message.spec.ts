1fbd1710acbf22c13509e62f7779a94e34988c00
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-12-10 23:30:41 -0300

    test: ensure DbGetMessage call getMessageRepository with correct values

---
+++ tests/data/usecase/get-message.spec.ts
@@ -0,0 +1,53 @@
+import type { GetMessageInput } from "./../../../src/domain/usecase/get-message";
+import { DbGetMessage } from "../../../src/data/usecase/get-message";
+import type {
+  GetMessageRepository,
+  GetMessageRepositoryInput,
+} from "../../../src/data/protocols/get-message-repository";
+import type { MessagesEntity } from "../../../src/infra/db/postgreSQL/entities/message-postgresql-entity";
+import { ChatEntity } from "../../../src/infra/db/postgreSQL/entities/chat-postgresql-entity";
+
+const makeGetMessageRepositoryStub = (): GetMessageRepository => {
+  class GetMessageRepositoryStub implements GetMessageRepository {
+    public async get(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      data: GetMessageRepositoryInput
+    ): Promise<Array<MessagesEntity>> {
+      return [
+        {
+          chat: new ChatEntity(),
+          message: "hello",
+          userId: "fake-user-id",
+          userName: "fake-user-name",
+        },
+      ];
+    }
+  }
+
+  return new GetMessageRepositoryStub();
+};
+
+const makeSut = () => {
+  const getMessageRepository = makeGetMessageRepositoryStub();
+  const sut = new DbGetMessage(getMessageRepository);
+
+  return {
+    sut,
+    getMessageRepository,
+  };
+};
+
+const fakeParams: GetMessageInput = {
+  key: "fake-key",
+};
+
+describe("GetMessage Database", () => {
+  test("should call getMessageRepository with correct values", async () => {
+    const { sut, getMessageRepository } = makeSut();
+    const getSpy = jest.spyOn(getMessageRepository, "get");
+
+    await sut.get(fakeParams);
+
+    expect(getSpy).toBeCalledWith(fakeParams);
+  });
+});
