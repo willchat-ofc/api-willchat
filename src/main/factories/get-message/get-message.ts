080bc7e5db643d102e0069de164b130946dd97ef
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-12-12 00:02:24 -0300

    feature: make factorie of GetMessageController

---
+++ src/main/factories/get-message/get-message.ts
@@ -0,0 +1,15 @@
+import { DbGetMessage } from "../../../data/usecase/get-message";
+import { GetMessagePostgreRepository } from "../../../infra/db/postgreSQL/repositories/get-message-repository";
+import { GetMessageController } from "./../../../presentation/controller/get-message";
+import { makeGetMessageValidation } from "./get-message-validation";
+
+export const makeGetMessageController = (): GetMessageController => {
+  const getMessageRepository = new GetMessagePostgreRepository();
+  const dbGetMessage = new DbGetMessage(getMessageRepository);
+  const messageController = new GetMessageController(
+    makeGetMessageValidation(),
+    dbGetMessage
+  );
+
+  return messageController;
+};
--- tests/main/factories/get-message-validation.spec.ts
+++ tests/main/factories/get-message-validation.spec.ts
@@ -1,16 +1,16 @@
 import { RequiredFieldValidation } from "../../../src/presentation/helpers/validators/required-field-validation/required-field-validation";
 import { ValidationComposite } from "../../../src/presentation/helpers/validators/validation-composite";
 import type { Validation } from "../../../src/presentation/protocols/validation";
-import { makeGetKeyValidation } from "../../../src/main/factories/get-key/get-key-validation";
+import { makeGetMessageValidation } from "../../../src/main/factories/get-message/get-message-validation";
 
 jest.mock("../../../src/presentation/helpers/validators/validation-composite");
 
 describe("GetKey Validation", () => {
   test("should call Validation composite with valid Validations", () => {
-    makeGetKeyValidation();
+    makeGetMessageValidation();
     const validations: Array<Validation> = [];
 
-    for (const field of ["accesstoken"]) {
+    for (const field of ["key"]) {
       validations.push(new RequiredFieldValidation(field));
     }
 
